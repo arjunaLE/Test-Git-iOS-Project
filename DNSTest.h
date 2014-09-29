775050db067b2312faa0aaf046f92565a671434e
developer <developer@developers-Mac.local>
2014-09-29 17:49:35 +0200

    added a test class

---
+++ DNSTest.h
@@ -0,0 +1,13 @@
+//
+//  DNSTest.h
+//  Git Test
+//
+//  Created by developer on 29.09.14.
+//  Copyright (c) 2014 dns-spirit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DNSTest : NSObject
+
+@end
---
+++ DNSTest.m
@@ -0,0 +1,13 @@
+//
+//  DNSTest.m
+//  Git Test
+//
+//  Created by developer on 29.09.14.
+//  Copyright (c) 2014 dns-spirit. All rights reserved.
+//
+
+#import "DNSTest.h"
+
+@implementation DNSTest
+
+@end
